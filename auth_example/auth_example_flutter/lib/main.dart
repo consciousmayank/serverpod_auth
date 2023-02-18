e980d681192811880dfbddbda86209a1759b2ceb
Mayank Joshi <consciousmayank@gmail.com>
2023-02-18 09:18:22 +0530

    Auth works

--- auth_example/auth_example_flutter/lib/main.dart
+++ auth_example/auth_example_flutter/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:auth_example_client/auth_example_client.dart';
 import 'package:flutter/material.dart';
+import 'package:serverpod_auth_email_flutter/serverpod_auth_email_flutter.dart';
+import 'package:serverpod_auth_shared_flutter/serverpod_auth_shared_flutter.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
 
 // Sets up a singleton client object that can be used to talk to the server from
@@ -7,10 +9,19 @@ import 'package:serverpod_flutter/serverpod_flutter.dart';
 // The client is set up to connect to a Serverpod running on a local server on
 // the default port. You will need to modify this to connect to staging or
 // production servers.
-var client = Client('http://localhost:8080/')
-  ..connectivityMonitor = FlutterConnectivityMonitor();
+Client client = Client(
+    'http://localhost:8080/',
+    authenticationKeyManager: FlutterAuthenticationKeyManager(),
+  );
 
-void main() {
+void main() async  {
+
+  SessionManager sessionManager = SessionManager(
+    caller: client.modules.auth,
+  );
+  await sessionManager.initialize();
+
+
   runApp(const MyApp());
 }
 
@@ -88,6 +99,7 @@ class MyHomePageState extends State<MyHomePage> {
                 child: const Text('Send to Server'),
               ),
             ),
+            SignInWithEmailButton(caller: client.modules.auth),
             _ResultDisplay(
               resultMessage: _resultMessage,
               errorMessage: _errorMessage,
--- auth_example/auth_example_server/lib/server.dart
+++ auth_example/auth_example_server/lib/server.dart
@@ -5,6 +5,8 @@ import 'package:auth_example_server/src/web/routes/root.dart';
 import 'src/generated/protocol.dart';
 import 'src/generated/endpoints.dart';
 
+import 'package:serverpod_auth_server/module.dart' as auth;
+
 // This is the starting point of your Serverpod server. In most cases, you will
 // only need to make additions to this file if you add future calls,  are
 // configuring Relic (Serverpod's web-server), or need custom setup work.
@@ -28,6 +30,19 @@ void run(List<String> args) async {
     RouteStaticDirectory(serverDirectory: 'static', basePath: '/'),
     '/*',
   );
+
+  auth.AuthConfig.set(auth.AuthConfig(
+    sendValidationEmail: (session, email, validationCode) async {
+      print('validationCode :: $validationCode');
+      print('email :: $email');
+      return true;
+    },
+    sendPasswordResetEmail: (session, userInfo, validationCode) async {
+      print('validationCode :: $validationCode');
+      print('UserInfo :: ${userInfo.toJson()}');
+      return true;
+    },
+  ));
 
   // Start the server.
   await pod.start();
